f6c2d3c78ed4a396e506675b4bf988b1df22b486
Aldair <aldairrr00@hotmail.com>
2023-08-22 18:50:27 -0500

    Cambios esteticos

--- clase7/tipcalculator.py
+++ clase7/tipcalculator.py
@@ -2,10 +2,10 @@
 ## entre cuantos
 ## propina
 def tipcalculator():
-    cuenta = float(input("cuanto fue?: "))
+    cuenta = float(input("Cuanto fue en total?: "))
     personas= float(input("Entre cuantos van a pagar?: "))
     pago_individual = cuenta/personas
-    propina=float(input("Cuanto quieres dejar de propina(0,5,10,12,15,20?: "))
+    propina=float(input("Cuanto quieres dejar de propina (10,12,15,20)?: "))
     pago_final= pago_individual * (1+propina/100)
     pago_final = round(pago_final,2)
     
