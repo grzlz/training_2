6e7307d82724e14c437ae1b3bf9ebb97cf7073f2
Aldair <aldairrr00@hotmail.com>
2023-08-22 18:52:46 -0500

    Ya listo

--- clase7/tipcalculator.py
+++ clase7/tipcalculator.py
@@ -2,13 +2,11 @@
 ## entre cuantos
 ## propina
 def tipcalculator():
-    cuenta = float(input("Cuanto fue en total?: "))
-    personas= float(input("Entre cuantos van a pagar?: "))
+    cuenta = float(input("Hola, ¿Cuanto fue en total?: "))
+    personas= float(input("¿Entre cuantos van a pagar?: "))
     pago_individual = cuenta/personas
-    propina=float(input("Cuanto quieres dejar de propina (10,12,15,20)?: "))
-    pago_final= pago_individual * (1+propina/100)
-    pago_final = round(pago_final,2)
-    
-    print(f"te toca pagar: ${pago_final}" ) 
+    propina=float(input("¿Cuanto quieres dejar de propina (10,12,15,20)?: "))
+    pago_final= round(pago_individual * (1+propina/100),2)
+    print(f"Te toca pagar: ${pago_final}" ) 
 
 tipcalculator() 
